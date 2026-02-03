704a4c7d9746fcedeccfc3f6f55eeb44f975c409
chhabihathel <chhabih.bba2023@ssism.org>
2026-02-03 21:45:25 +0530

    Update  App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,6 +30,8 @@ export default App;
 // export default function App() {
 //   return <AppRoutes />;
 // }
+
+
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
