e28221742acf26bd654585290d282bb4da3465fd
chhabihathel <chhabih.bba2023@ssism.org>
2026-02-06 00:53:19 +0530

    intigration dashboard API

--- backend/src/controllers/ai.controller.js
+++ backend/src/controllers/ai.controller.js
@@ -8,9 +8,9 @@ const { getMedicineExplanation } = require("../services/ai.service");
 
 exports.manualSearch = async (req, res) => {
     try {
-        const { text } = req.body;
-        const userId = req.user.id;
-
+        const { name } = req.body;
+        const userId = req.user._id;
+         const text = name;
         const normalized = normalizeText(text);
 
         let scanMed = await ScanMedicine.findOne({
@@ -68,7 +68,7 @@ exports.manualSearch = async (req, res) => {
         console.error(error);
 
         await AIHistory.create({
-            userId: req.user.id,
+            userId: req.user._id,
             inputText: req.body.text,
             queryType: "text",
             status: "failed"
--- frontend/src/services/authApi.js
+++ frontend/src/services/authApi.js
@@ -2,7 +2,7 @@ import axios from "./axios";
 
 // -----LOGIN API-----
 export const loginUser = (email, password) => {
-  return axios.post("/api/auth/login", {
+  return axios.post("/auth/login", {
     email,
     password,
   });
@@ -10,10 +10,22 @@ export const loginUser = (email, password) => {
 
 // -----SIGNUP API-----
 export const signupUser = (name, email, age, password) => {
-  return axios.post("/api/auth/signup", {
+  return axios.post("/auth/signup", {
     name,
     email,
     age,
     password,
   });
 };
+
+// ------Profile Api-------
+
+export const profileUser = (name, email, phone, age) => {
+  return axios.post("/auth/profile", {
+    name,
+    email,
+    phone,
+    age,
+  });
+};
+
