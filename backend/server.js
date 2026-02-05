e58e955d62b8031f15f5647818b020b7bd6862a2
lakshman5615 <lakshmanjoshiji@gmail.com>
2026-02-05 11:43:01 +0530

    dashboard updated and  landing page

--- backend/server.js
+++ backend/server.js
@@ -6,43 +6,19 @@ const User = require("./src/models/User");
 dotenv.config();
 const authRoutes = require("./src/routes/auth.routes");
 const aiRoutes = require("./src/routes/ai.routes");
-const helmet = require('helmet');
 const dashboardRoutes = require("./src/routes/dashboard.routes")
 
-// Cron jobs
-require('./src/cron/medicine-reminder.cron');
-require('./src/cron/alerts.cron');
-
-// Import routes
-const medicineRoutes = require('./src/routes/medicine.routes');
-const medicineActionRoutes = require('./src/routes/medicine-actions.routes');
-const doseRoutes = require('./src/routes/dose.routes');
-const notificationRoutes = require('./src/routes/notification.routes');
-const phoneUserRoutes = require('./src/routes/phone-user.routes');
-const reminderRoutes = require('./src/routes/reminder.routes');
 
 
 const app = express();
+
+// middlewares
+app.use(cors());
 app.use(express.json());
 
 // app.use(express.urlencoded({ extended: true }));
 
-// app.use(express.urlencoded({ extended: true }));
-
-
-
-
-// 🔐 SECURITY FIRST
-app.use(helmet());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
-
-// Root route
-app.get('/', (req, res) => res.send('Cabinet API is running 🚀'));
-
-// Connect MongoDB
+// connect DB
 connectDB();
 
 // root test
@@ -50,22 +26,17 @@ app.get("/", (req, res) => {
   res.send("Mediscan Backend is running ");
 });
 app.use("/auth", authRoutes);
-
-// Use routes
-app.use('/api/auth', authRoutes);
-app.use('/api/ai', aiRoutes);
-app.use('/api/medicine', medicineRoutes);
-app.use('/api/medicine-action', medicineActionRoutes);
-app.use('/api/dose', doseRoutes);
-app.use('/api/notification', notificationRoutes);
-app.use('/api/phone-user', phoneUserRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/dashboard", dashboardRoutes);
-app.use('/api/reminder', reminderRoutes);
 
 
 
-
-// Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Cabinet service running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+
+
