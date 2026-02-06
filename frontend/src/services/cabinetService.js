929daeea4b7b7487e9340d8f0bacb82ce0f9f8e2
SiddhikaBishnoi276 <bishnoisiddhika@gmail.com>
2026-02-06 11:12:28 +0530

    cabinet change api

--- frontend/src/services/cabinetService.js
+++ frontend/src/services/cabinetService.js
@@ -3,7 +3,7 @@ import axiosInstance from './axios';
 // Add medicine to cabinet
 export const addToCabinet = async (medicineData) => {
   try {
-    const response = await axiosInstance.post('/cabinet/add', medicineData);
+    const response = await axiosInstance.post('/api/medicine/add', medicineData);
     return response.data;
   } catch (error) {
     throw error.response?.data || error.message;
@@ -13,7 +13,7 @@ export const addToCabinet = async (medicineData) => {
 // Get user's cabinet medicines
 export const getCabinet = async () => {
   try {
-    const response = await axiosInstance.get('/cabinet/my');
+    const response = await axiosInstance.get('/api/medicine/');
     return response.data;
   } catch (error) {
     throw error.response?.data || error.message;
